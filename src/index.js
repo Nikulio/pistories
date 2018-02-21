4a796af44bfc84a155dd9d02bb358a91b88fe689
Nikita Shchypylov <sn97@icloud.com>
2018-02-21 08:59:08 +0100

    first

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import FormContainer from "./containers/FormContainer";
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+    <FormContainer />, document.getElementById('root')
+)
